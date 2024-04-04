21ac94baaa25512e2be90aef0e80b84f0b803162
MaheshB77 <mbansode7696@gmail.com>
2024-04-05 00:15:19 +0530

    Added some styling to summary page

--- lib/quiz_app/components/answer_details.dart
+++ lib/quiz_app/components/answer_details.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:second_app/quiz_app/models/question_summary.dart';
 import 'package:second_app/shared/widgets/spacing.dart';
 
@@ -8,26 +6,55 @@ class AnswerDetails extends StatelessWidget {
   final List<QuestionSummary> questionSummary;
   const AnswerDetails({super.key, required this.questionSummary});
 
+  Color getChipColor(QuestionSummary summary) {
+    Color chipColor;
+    if (summary.isCorrect) {
+      chipColor = const Color.fromARGB(255, 164, 90, 206);
+    } else {
+      chipColor = const Color.fromARGB(255, 222, 92, 54);
+    }
+    return chipColor;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: questionSummary.map((e) {
-        return Row(
-          children: [
-            Text((e.questionIndex + 1).toString()),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(e.question),
-                  verticalSpace(5),
-                  Text(e.userAnswer),
-                  Text(e.correctAnswer),
-                ],
-              ),
-            ),
-          ],
-        );
-      }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: questionSummary.map((e) {
+            return Row(
+              verticalDirection: VerticalDirection.up,
+              children: [
+                Container(
+                  width: 30.0,
+                  height: 30.0,
+                  decoration: BoxDecoration(
+                    color: getChipColor(e),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Text(
+                      (e.questionIndex + 1).toString(),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(e.question),
+                      verticalSpace(5),
+                      Text(e.userAnswer),
+                      Text(e.correctAnswer),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
