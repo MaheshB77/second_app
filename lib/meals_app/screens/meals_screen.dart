a007a9425104684d77f7ebdaece3398b7eeb67eb
MaheshB77 <mbansode7696@gmail.com>
2024-04-14 10:13:23 +0530

    Added meal item widget

--- lib/meals_app/screens/meals_screen.dart
+++ lib/meals_app/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:second_app/meals_app/models/meal.dart';
+import 'package:second_app/meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   final String title;
@@ -15,9 +16,8 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => Text(
-        meals[index].title,
-        style: getBodyTextStyle(context),
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
       ),
     );
 
---
+++ lib/meals_app/widgets/meal_item_trait.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MealItemTrait extends StatelessWidget {
+  final IconData icon;
+  final String description;
+  const MealItemTrait({
+    super.key,
+    required this.icon,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon),
+        const SizedBox(width: 4),
+        Text(
+          description,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const SizedBox(width: 16)
+      ],
+    );
+  }
+}
