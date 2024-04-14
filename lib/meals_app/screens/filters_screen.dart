8f0cc1e006a4dc41913a82da1726027d148a38ee
MaheshB77 <mbansode7696@gmail.com>
2024-04-15 02:02:12 +0530

    Added more filters

--- lib/meals_app/screens/filters_screen.dart
+++ lib/meals_app/screens/filters_screen.dart
@@ -9,39 +9,103 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   bool _glutenFreeFilter = false;
+  bool _lactoseFreeFilter = false;
+  bool _vegetarianFilter = false;
+  bool _veganFilter = false;
+
+  void _updateFilter(bool isActive, String filter) {
+    if (filter == 'Gluten-Free') {
+      setState(() {
+        _glutenFreeFilter = isActive;
+      });
+    } else if (filter == 'Lactose-Free') {
+      setState(() {
+        _lactoseFreeFilter = isActive;
+      });
+    } else if (filter == 'Vegetarian') {
+      setState(() {
+        _vegetarianFilter = isActive;
+      });
+    } else if (filter == 'Vegan') {
+      setState(() {
+        _veganFilter = isActive;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your filters'),
       ),
       body: Column(
         children: [
-          SwitchListTile(
+          FilterTile(
             value: _glutenFreeFilter,
-            onChanged: (isActive) {
-              setState(() {
-                _glutenFreeFilter = isActive;
-              });
-            },
-            title: Text(
-              'Gluten-Free',
-              style: theme.textTheme.titleLarge!.copyWith(
-                color: theme.colorScheme.onBackground,
-              ),
-            ),
-            subtitle: Text(
-              'Only include gluten-free meals',
-              style: theme.textTheme.labelMedium!.copyWith(
-                color: theme.colorScheme.onBackground,
-              ),
-            ),
-            activeColor: theme.colorScheme.tertiary,
+            title: 'Gluten-Free',
+            subtitle: 'Only include gluten-free meals',
+            onUpdateFilter: _updateFilter,
+          ),
+          FilterTile(
+            value: _lactoseFreeFilter,
+            title: 'Lactose-Free',
+            subtitle: 'Only include lactose-free meals',
+            onUpdateFilter: _updateFilter,
+          ),
+          FilterTile(
+            value: _vegetarianFilter,
+            title: 'Vegetarian',
+            subtitle: 'Only include vegetarian meals',
+            onUpdateFilter: _updateFilter,
+          ),
+          FilterTile(
+            value: _veganFilter,
+            title: 'Vegan',
+            subtitle: 'Only include vegan meals',
+            onUpdateFilter: _updateFilter,
           ),
         ],
       ),
+    );
+  }
+}
+
+class FilterTile extends StatelessWidget {
+  final bool value;
+  final String title;
+  final String subtitle;
+  final void Function(bool isActive, String filter) onUpdateFilter;
+
+  const FilterTile({
+    super.key,
+    required this.value,
+    required this.title,
+    required this.subtitle,
+    required this.onUpdateFilter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return SwitchListTile(
+      value: value,
+      onChanged: (isActive) {
+        onUpdateFilter(isActive, title);
+      },
+      title: Text(
+        title,
+        style: theme.textTheme.titleLarge!.copyWith(
+          color: theme.colorScheme.onBackground,
+        ),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: theme.textTheme.labelMedium!.copyWith(
+          color: theme.colorScheme.onBackground,
+        ),
+      ),
+      activeColor: theme.colorScheme.tertiary,
     );
   }
 }
