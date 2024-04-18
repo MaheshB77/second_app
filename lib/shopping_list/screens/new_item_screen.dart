afee89f6cbc3a65c3758706ee2ae3da4b01d854c
MaheshB77 <mbansode7696@gmail.com>
2024-04-18 23:51:32 +0530

    Added fields to new item screen

--- lib/shopping_list/screens/new_item_screen.dart
+++ lib/shopping_list/screens/new_item_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/shopping_list/data/categories.dart';
 
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
@@ -14,9 +15,63 @@ class _NewItemScreenState extends State<NewItemScreen> {
       appBar: AppBar(
         title: const Text('Add new item'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
-        child: Text('The form'),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demo msg';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        return 'Demo msg';
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 16,
+                                  width: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 10),
+                                Text(category.value.title)
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
