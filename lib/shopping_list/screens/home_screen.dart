715ebaf8591e6ac82e2f8e69c49472b979f4c35b
MaheshB77 <mbansode7696@gmail.com>
2024-04-18 20:30:56 +0530

    Added new item screen

--- lib/shopping_list/screens/home_screen.dart
+++ lib/shopping_list/screens/home_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/shopping_list/screens/new_item_screen.dart';
 import 'package:second_app/shopping_list/widgets/grocery_list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void _addItem() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => const NewItemScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: const GroceryList(),
     );
--- lib/shopping_list/widgets/grocery_list.dart
+++ lib/shopping_list/widgets/grocery_list.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:second_app/shopping_list/data/dummy_items.dart';
-import 'package:second_app/shopping_list/widgets/grocery_item_w.dart';
 
 class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: groceryItems
-          .map(
-            (item) => GroceryItemW(groceryItem: item),
-          )
-          .toList(),
+    return ListView.builder(
+      itemCount: groceryItems.length,
+      itemBuilder: (ctx, index) => ListTile(
+        leading: Container(
+          height: 20,
+          width: 20,
+          decoration: BoxDecoration(
+            color: groceryItems[index].category.color,
+          ),
+        ),
+        title: Text(groceryItems[index].name),
+        trailing: Text(groceryItems[index].quantity.toString()),
+      ),
     );
   }
 }
